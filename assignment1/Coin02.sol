d73152f6182dd1914fbe33c112827641448f2b31
Ma Hui <huima22@gmail.com>
2021-01-24 09:20:05 +0800

    initial commit of first version

--- assignment1/Coin02.sol
+++ assignment1/Coin02.sol
@@ -1,43 +1,49 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0 <0.7.0; 
+pragma solidity >=0.5.0 <0.7.0;
 
-contract Coin { 
-  address public minter; 
-  mapping (address => Player) public players; 
-  address [] playerAddress;
-  
-  struct Player {
-      uint balance;
-      string name;
-      string phoneNumber;
-  }
- 
-  constructor( ) public { 
-    minter = msg.sender; 
-  }
-  
-  function addPlayer(address playAddress,string memory name, string memory phoneNumber) internal{
-      playerAddress.push(playAddress);
-      players[playAddress] = Player(0,name,phoneNumber);
-  }
-  
-  
-  function mint(address receiver, uint amount) public onlyMinter { 
-    players[receiver].balance += amount; 
-  } 
-  
-  modifier onlyMinter() {
-      require(msg.sender == minter, "Only minter can mint coin." ); 
-      _;
-  }
- 
-  function send(address receiver, uint amount) public checkIfBalanceSufficient(amount) { 
-    players[msg.sender].balance -= amount; 
-    players[receiver].balance += amount; 
-  } 
-  
-  modifier checkIfBalanceSufficient(uint amount) {
-       require(amount <= players[msg.sender].balance, "Insufficient balance."); 
-       _;
-  }
+contract Coin {
+    address public minter;
+    mapping(address => User) public user;
+    address [] userAddresses;
+
+    struct User {
+        uint balance;
+        string name;
+        uint phoneNumber;
+    }
+
+    constructor() public {
+        minter = msg.sender;
+    }
+
+    function addUser(address userAddress, string memory name, uint phoneNumber) public {
+        require(phoneNumber > 9999999 && phoneNumber < 100000000, "Please enter a valid Singapore number without country code.");
+        userAddresses.push(userAddress);
+        user[userAddress] = User(0, name, phoneNumber);
+    }
+
+    function mint(address receiver, uint amount) public onlyMinter {
+        user[receiver].balance += amount;
+    }
+
+    modifier onlyMinter() {
+        require(msg.sender == minter, "Only minter can mint coin.");
+        _;
+    }
+
+    function send(address receiver, uint amount) public checkIfUserExist(receiver) checkIfBalanceSufficient(amount) {
+        user[msg.sender].balance -= amount;
+        user[receiver].balance += amount;
+    }
+
+    modifier checkIfBalanceSufficient(uint amount) {
+        require(amount <= user[msg.sender].balance, "Insufficient balance.");
+        _;
+    }
+
+    modifier checkIfUserExist(address receiver) {
+        require(user[receiver].phoneNumber != 0, "User not yet added to friends");
+        //phoneNumber default to 0 but not possible during creation
+        _; //stacked modifier
+    }
 }
