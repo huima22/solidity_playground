53d6a92ee3a38721487d63e55ef2ca1cf3e5fafa
Ma Hui <huima22@gmail.com>
2021-01-24 23:01:13 +0800

    update the constructor

--- assignment1/Coin05.sol
+++ assignment1/Coin05.sol
@@ -17,6 +17,7 @@ contract Coin {
     constructor( ) public { 
         minter = msg.sender; 
         users[msg.sender] = User(0, "minter", 99999999); //initialise the contract creator
+		userAddresses.push(msg.sender);
     }
   
     function addUser(address userAddress, string memory name, uint phoneNumber) public {
